219dc2d7be81a271a1d050aa6102ff637d8a4072
And24reas <meinakkount@gmail.com>
2022-02-12 00:36:38 +0100

    almost

--- csv_formatter.cpp
+++ csv_formatter.cpp
@@ -1,6 +1,3 @@
-// csv_formatter.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include <stdio.h>
 #include <string>
@@ -9,51 +6,54 @@
 #pragma warning(suppress : 4996)
 
 struct Table_entry {
-    std::string name;
-    std::string surname;
-    std::string age;
-    std::string address;
-    std::string plz;
-    std::string phone_no;
+    std::string name, surname, age,address, plz, phone_no;
+    
 };
 
-//std::vector<Table_entry> fill_up_array(vector<string> &data_to_store, File &fp) {}
-/*
-std::ostream& operator<<(ostream& stream, const Table_entry& table_entry) {
-    stream << table_entry.name << " | " << table_entry.surname
-        << " | " << table_entry.age << " | " << table_entry.age
-        << " | " << table_entry.address << " | " << table_entry.plz
-        << " | " << table_entry.phone_no << endl;
 
+std::ostream& operator<<(std::ostream& stream, const Table_entry& table_entry) {
+    stream << table_entry.name << " ," << table_entry.surname
+        << " ," << table_entry.age << " ," << table_entry.address << " ," << table_entry.plz
+        << " ,"<< table_entry.phone_no << std::endl;
+    return stream;
 }
-*/
+
+
 
 int main()
 {
     std::vector<Table_entry> table_entries;
     std::vector<std::string> data_to_store;
     
+    
     FILE* fp;
     char c=NULL;
-    fp=fopen("data.csv","r");
+    fp=fopen("data1.csv","r");
     std::string data="";
     while (c != EOF) {
         c = fgetc(fp);
         data.push_back(c); 
+        printf("%c", c);
     }
     fclose(fp);
-    //data_to_store.push_back(current);
+    
     std::stringstream s(data);
 
     while (std::getline(s, data, ';')) {
         data_to_store.push_back(data);
     }
     
-    for (std::string s : data_to_store) {
-        std::cout << s << std::endl;
+    /*
+    for (int i = 0; i < (int) data_to_store.size(); i+=6) {
+     
+        table_entries.push_back({data_to_store[i], data_to_store[i+1], data_to_store[i+2], data_to_store[i+3], data_to_store[i+4], data_to_store[i+5] });  
     }
-    
-    
+    */
+    for (Table_entry t : table_entries) {
+       
+        std::cout << t << std::endl;
+    }
+   
     return 0;
 
 }
